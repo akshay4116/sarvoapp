545b5e610c07651c9ae625cdf9f77417d9f4990d
akshay4116 <akshaybhat4116@gmail.com>
2025-04-13 18:36:03 +0530

    master commit

---
+++ sarvottama-app/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const [message, setMessage] = useState('Loading...');
+
+  useEffect(() => {
+    fetch('http://192.168.29.231:3000') // Replace with your IPv4 address
+      .then(res => res.text())
+      .then(setMessage)
+      .catch(err => setMessage('Error: ' + err.message));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>{message}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
